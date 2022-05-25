93328c7b4a7d7204118604997ced341648dc4589
Mumani <yb2316@naver.com>
2022-05-25 21:25:48 +0900

    :tada: Create a project

---
+++ SwiftUI/ReviewWQuestion-Calculator/ReviewWQuestion_CalculatorApp.swift
@@ -0,0 +1,15 @@
+//
+//  ReviewWQuestion_CalculatorApp.swift
+//  ReviewWQuestion-Calculator
+//
+
+import SwiftUI
+
+@main
+struct ReviewWQuestion_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
